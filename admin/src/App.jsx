41b059c7be189f16e72e932637f0dfc043d0e1f0
Rahulkrpd <87600436+Rahulkrpd@users.noreply.github.com>
2024-09-23 20:58:23 -0700

    Update App.jsx
    change backend url

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -9,7 +9,7 @@ import 'react-toastify/dist/ReactToastify.css';
 
 
 const App = () => {
-  const url = "https://food-del-backend-6b79.onrender.com";
+  const url = "https://food-del-backend-24sk.onrender.com";
   return (
     <div>
       <ToastContainer />
